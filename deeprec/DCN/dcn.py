ab661b845b70f90d7b2959aa4e58d5a464a5b0d6
Cecilio-Jia <502205863@qq.com>
2019-04-02 15:58:33 +0800

    Create DCN

---
+++ deeprec/DCN/dcn.py
@@ -0,0 +1,12 @@
+"""
+Created on 2019/04/02 15:56
+author: Tong Jia
+email: cecilio.jia@gmail.com
+description:
+    An implementation of Deep & Cross Network for Ad Click Predictions (DCN).
+    See algorithm and hyperparameter details:
+        [Wang et al., 2017](https://arxiv.org/pdf/1708.05123.pdf)
+    The algorithm is developed with TensorFlow Estimator based on TensorFlow 1.12.0 version.
+"""
+import tensorflow as tf
+
