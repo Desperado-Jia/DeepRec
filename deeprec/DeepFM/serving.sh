aabd7302215eabad9af6dd4e2bcf0a6f04965e84
Cecilio-Jia <502205863@qq.com>
2019-04-04 00:07:46 +0800

    initial commit

--- deeprec/DeepFM/serving.sh
+++ deeprec/DeepFM/serving.sh
@@ -4,4 +4,5 @@ cd /Users/ceciliojia/Desktop/ML-System/DeepRec/deeprec/DeepFM
 
 docker run -p 8501:8501 \
 --mount type=bind,source=./export/,target=/models/DeepFM \
--e MODEL_NAME=DeepFM -t tensorflow/serving
+-e MODEL_NAME=DeepFM \
+-t tensorflow/serving
